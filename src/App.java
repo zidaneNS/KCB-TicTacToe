ec127ca7f5e84810e89e0e70653cfea3e155d7f9
Muhammad Zidane <zidane.nur004@gmail.com>
2024-08-19 12:56:14 +0700

    test pull di git

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("test 2");
         System.out.println("Test");
+        System.out.println("Test lg");
         new Main();
     }
 }
